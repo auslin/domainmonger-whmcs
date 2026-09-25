5331714b09138ed8d772e3e5e6ee1673b8bc6549
Austin <alinford@gmail.com>
2026-09-25 09:21:57 -0600

    Cleanup retired no-op WHMCS hooks

--- includes/hooks/domainmonger_epp_route_probe_966.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * DomainMonger cleanup placeholder.
- * This file intentionally registers no hooks. It neutralizes failed EPP experiments.
- */
-if (!defined('WHMCS')) {
-    die('This file cannot be accessed directly');
-}
--- includes/hooks/domainmonger_register_patch223.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Retired by DomainMonger v8x Register Patch 271 Patch-221 rollback test.
-if (!defined('WHMCS')) { exit; }
